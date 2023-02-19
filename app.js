76cec91bab116685f6639804f29f0b9f162f2de1
himan <himanshu.rao.075@gmail.com>
2023-02-20 01:04:26 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public-flutter')));
+app.listen(3049, () => {
+    console.log(`App running on port 3012!`)
+})
+
+module.exports = app;
